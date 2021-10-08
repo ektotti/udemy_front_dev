3ecdf21e152029342ff8cb9530ddee8a19730f89
github user name <ektotti@gmail.com>
2021-10-08 09:16:39 +0900

    070_Swiperを使った・・・・途中

--- 05_JavaScriptを実践で使う/030_スクロール監視クラスの作成/start/main.js
+++ 05_JavaScriptを実践で使う/030_スクロール監視クラスの作成/start/main.js
@@ -1,57 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const cb = function(el, isIntersecting){
-        const ta = new TextAnimation(el);
         if(isIntersecting){
+            const ta = new TextAnimation(el);
             ta.animate();
         }
     }   
     const so = new ScrollObserber('.animate-title', cb)
 });
-
-// const cb = (entries, observer)=>{
-//     entries.forEach(entry=>{
-//         if(entry.isIntersecting){
-//             const ta = new TextAnimation(entry.target);
-//             ta.animate();
-//             observer.unobserve(entry.target);
-//         }
-//     })
-// }
-// const options = {
-//     root:null,
-//     rootMargin: '0px',
-//     threshold:0
-// }
-
-
-class ScrollObserber{
-    constructor(els, cb, options){
-       this.els =  document.querySelectorAll(els);
-       this.cb = cb;
-       const defaultOptions = {
-        root:null,
-        rootMargin: '0px',
-        threshold:0
-        }
-        this.options = Object.assign(defaultOptions, options);
-       this._init();
-    }
-    _init(){
-        const callback = (entries, observer)=>{
-            entries.forEach(entry=>{
-                if(entry.isIntersecting){
-                    this.cb(entry.target, true);
-                    observer.unobserve(entry.target);
-                }else{
-                    this.cb(entry.target, false);
-                }
-            });
-        };
-        this.io = new IntersectionObserver(callback.bind(this), this.options);
-        this.els.forEach(el =>{
-            this.io.observe(el);
-        } )
-    }
-
-
-}
--- 05_JavaScriptを実践で使う/040_演習_スクロール監視×スライドアニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/040_演習_スクロール監視×スライドアニメーション/start/main.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const cb = function (el, isIntersecting) {
         if(isIntersecting) {
-            const ta = new TextAnimation(el);
-            ta.animate();
+            el.classList.toggle('inview');
         }
+    
     }
 
-    const so = new ScrollObserver('.animate-title', cb);
+    const so = new ScrollObserver('.cover-slide', cb);
 });
 
